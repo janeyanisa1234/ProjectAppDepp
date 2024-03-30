855ccb5d58b329844445edf34f644ea84ef03b45
janeyanisa1234 <129841440+janeyanisa1234@users.noreply.github.com>
2024-03-30 17:45:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let currCity = "Bangkok";
 let units = "metric";
 
+let city = document.querySelector(".weather__city");
+let datetime = document.querySelector(".weather__datetime");
+let weather__forecast = document.querySelector('.weather__forecast');
+let weather__temperature = document.querySelector(".weather__temperature");
+let weather__icon = document.querySelector(".weather__icon");
+let weather__minmax = document.querySelector(".weather__minmax")
+let weather__realfeel = document.querySelector('.weather__realfeel');
+let weather__humidity = document.querySelector('.weather__humidity');
+let weather__wind = document.querySelector('.weather__wind');
+let weather__pressure = document.querySelector('.weather__pressure');
+
 
 
 document.querySelector(".weather__search").addEentListener('submit', e => {
